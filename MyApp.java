62bf588ed8d3a14395eb5a7e6fd836243859eba4
Artti Paasonen <t7paar00@students.oamk.fi>
2018-09-19 13:27:58 +0300

    Fixed a bug

--- MyApp.java
+++ MyApp.java
@@ -1,4 +1,4 @@
-public class main {
+class main {
 	public static void main(String[] args) {
 		Product product = new Product("Edam", 3.3, 120);
 		System.out.println("Product value is " + product.countValue());
